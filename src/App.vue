1a5242fccccfd34edab15e7f3e4b73235787dafc
Ahmad Nurul Laiq <ahmadlaiq97@gmail.com>
2024-06-24 13:25:10 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>Vue 3 + Vite + Tailwind CSS</h1>
+  </div>
+</template>
+
+<style scoped>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+  h1 {
+    font-size: 2rem;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+//import createApp from Vue
+import { createApp } from 'vue';
+
+//import component App
+import App from './App.vue';
+
+//create App Vue
+const app = createApp(App);
+
+app.mount('#app');
